4f8c71b503758e723b6e5af0876645678bcf027a
Joy Kim <nietzsche21st@gmail.com>
2023-01-04 14:34:17 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:state_notifier_ex/providers/bg_color.dart';
+import 'package:state_notifier_ex/providers/counter.dart';
+import 'package:state_notifier_ex/providers/customer_level.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        StateNotifierProvider<BgColor, BgColorState>(
+          create: (create) => BgColor(),
+        ),
+        StateNotifierProvider<Counter, CounterState>(
+          create: (create) => Counter(),
+        ),
+        StateNotifierProvider<CustomerLevel, Level>(
+          create: (context) => CustomerLevel(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'State Notifier',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          useMaterial3: true,
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorState = context.watch<BgColorState>();
+    final counterState = context.watch<CounterState>();
+    final levelState = context.watch<Level>();
+    return Scaffold(
+      backgroundColor: levelState == Level.bronze
+          ? Colors.white
+          : levelState == Level.silver
+              ? Colors.grey
+              : Colors.yellow,
+      appBar: AppBar(
+        backgroundColor: colorState.color,
+        title: Text('AA'),
+      ),
+      body: Center(
+        child: Text(
+          '${counterState.counter}',
+          style: TextStyle(fontSize: 40),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            tooltip: 'Increment',
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          FloatingActionButton(
+            tooltip: 'Change color',
+            onPressed: () {},
+            child: Icon(Icons.color_lens),
+          ),
+        ],
+      ),
+    );
+  }
+}
